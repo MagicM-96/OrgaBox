a2fe89a784b4212b43fb08feaaa2a7995ae7494f
Erik Merlin Baudert (edu2fe) <ErikMerlin.Baudert@de.bosch.com>
2019-08-27 15:19:35 +0200

    Added actions in the item list

--- src/components/item.vue
+++ src/components/item.vue
@@ -13,7 +13,28 @@
         <td>{{ storeItems[item].title }}</td>
         <td>{{ storeItems[item].description }}</td>
         <td>{{ storeItems[item].stock }}</td>
-        <td><v-icon>mdi-menu</v-icon></td>
+        <td>
+          <v-menu>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                v-on="on"
+              >
+                <v-icon>mdi-menu</v-icon>
+              </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item
+                v-for="(action, i) in actions"
+                :key="i"
+                @click="action.callback(item)"
+              >
+                <v-list-item-title>{{ action.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </td>
       </tr>
     </tbody>
   </v-simple-table>
@@ -23,13 +44,30 @@
 <script>
 export default {
   props: ['items'],
+  data () {
+    return {
+      actions: [
+        { title: 'Edit', callback: (payload) => { this.edit(payload) } },
+        { title: 'Move', callback: (payload) => { this.move(payload) } },
+        { title: 'Delete', callback: (payload) => { this.delete(payload) } }
+      ]
+    }
+  },
   computed: {
     storeItems: function () {
       return this.$store.state.items
     }
   },
   methods: {
-
+    edit (itemId) {
+      console.log('editing', itemId)
+    },
+    delete (itemId) {
+      console.log('deleting', itemId)
+    },
+    move (itemId) {
+      console.log('moving', itemId)
+    }
   }
 }
 </script>
