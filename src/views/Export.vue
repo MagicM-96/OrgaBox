46aa15425bc97bdc077a95a59bc2124fde7d020b
mbaudert <rickba96@gmail.com>
2019-09-26 16:38:37 +0200

    Added Export/Import * first implementation of the function * JSON format * Still needs error handling on import and copy function on export

--- src/views/Export.vue
+++ src/views/Export.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <h1>Export your data</h1>
+    <v-select
+      v-model="format"
+      :items="availableFormats"
+      label="Export Format"
+    ></v-select>
+    <v-btn :disabled="format === undefined" @click="createExport()">Create Export</v-btn>
+    <template v-if="exportText">
+      <v-textarea v-model="exportText" label="Export"></v-textarea>
+      <v-btn @click="copyExport()">Copy to Clipboard</v-btn>
+    </template>
   </div>
 </template>
 
@@ -8,7 +18,36 @@
 export default {
   data () {
     return {
-
+      format: undefined,
+      availableFormats: [
+        {
+          text: 'JSON',
+          value: 0
+        }
+      ],
+      exportText: ''
+    }
+  },
+  methods: {
+    createExport () {
+      if (this.format !== undefined) {
+        let text = ''
+        switch (this.format) {
+          case 0: // JSON
+            text += '{"items":'
+            text += JSON.stringify(this.$store.state.items)
+            text += ',"boxes":'
+            text += JSON.stringify(this.$store.state.boxes)
+            text += '}'
+            break
+          default:
+            console.error('Unknown Export format!')
+        }
+        this.exportText = text
+      }
+    },
+    copyExport () {
+      // TODO: add a copy to Clipboard method
     }
   }
 }
--- src/views/Import.vue
+++ src/views/Import.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h1>Import your data</h1>
+    <v-select
+      v-model="format"
+      :items="availableFormats"
+      label="Export Format"
+    ></v-select>
+    <v-textarea v-model="importText" label="Import"></v-textarea>
+    <v-btn :disabled="!importText || format === undefined" @click="importData()">Import</v-btn>
   </div>
 </template>
 
@@ -8,7 +15,40 @@
 export default {
   data () {
     return {
-
+      format: undefined,
+      availableFormats: [
+        {
+          text: 'JSON',
+          value: 0
+        }
+      ],
+      importText: ''
+    }
+  },
+  methods: {
+    importData () {
+      if (this.format !== undefined && this.importText) {
+        let importData = {}
+        switch (this.format) {
+          case 0: // JSON
+            try {
+              importData = JSON.parse(this.importText)
+              // TODO: control if the JSON format is correct
+            } catch (e) {
+              console.error('Something went wrong on import!', e)
+              // TODO: Add an error report to the user
+              alert('Failed to parse import!')
+            }
+            break
+          default:
+            console.error('Unknown Export format!')
+        }
+        this.$store.commit('loadSave', {
+          items: importData.items || {},
+          boxes: importData.boxes || []
+        })
+        this.$router.replace('/')
+      }
     }
   }
 }
