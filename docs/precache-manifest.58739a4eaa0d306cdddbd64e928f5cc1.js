e5f4738ce9c83c75ebc285f6f3709946a7df4ad5
EMBaudert <abi2015-merlin@gmx.de>
2020-03-28 14:15:19 +0100

    Revert "Removed "docs" directory from Development branch"
    This reverts commit 17980f376b442c13277cdec55a8cd23dddf8eeca.

---
+++ docs/precache-manifest.58739a4eaa0d306cdddbd64e928f5cc1.js
@@ -0,0 +1,89 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "66cad96ea7cf75c4affe4b83f6efd424",
+    "url": "service-worker.js"
+  },
+  {
+    "revision": "140fe996d748ffbbe7cf1777ced447cf",
+    "url": "img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "ab92cd3b47d9b8a8603c",
+    "url": "js/app.59dcff79.js"
+  },
+  {
+    "revision": "39d6ba613b9a4380037e",
+    "url": "js/chunk-vendors.dbe852ae.js"
+  },
+  {
+    "url": "precache-manifest.945d2771f0629e6243e8a5de70b6fed9.js"
+  },
+  {
+    "revision": "ea58b03e753e8267afb980973871c954",
+    "url": "img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "581f8e57324ab5979edd",
+    "url": "js/about.00653a73.js"
+  },
+  {
+    "revision": "8008c876381d7b027eb12515c9ec4369",
+    "url": "img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "160bb1a889a190a98eb630ac238f7285",
+    "url": "img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "59eeee4f9204ba4dabf72a9572349e27",
+    "url": "img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "339680a3b1e3cc7ce7765a1c9a996ec6",
+    "url": "index.html"
+  },
+  {
+    "revision": "254361de448867f3e46281686fce79e9",
+    "url": "img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "25686b315c3e5c63f77f1529240b94fb",
+    "url": "img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "8008c876381d7b027eb12515c9ec4369",
+    "url": "img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "150fb57c53d5eddb9585092f4a0f884d",
+    "url": "img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "2114301831b2bf1512955b6e0dc6f516",
+    "url": "img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "ffbf7a145cad04a46529a58e7902d7d5",
+    "url": "img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "7022d31f2b9bb79ded16ac05d5ffdc95",
+    "url": "img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "789a5c889c5244caada3f81d67acefb4",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "39d6ba613b9a4380037e",
+    "url": "css/chunk-vendors.e611593c.css"
+  },
+  {
+    "revision": "ab92cd3b47d9b8a8603c",
+    "url": "css/app.f14b2702.css"
+  }
+];
---
+++ docs/precache-manifest.945d2771f0629e6243e8a5de70b6fed9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "39d6ba613b9a4380037e",
+    "url": "js/chunk-vendors.dbe852ae.js"
+  },
+  {
+    "revision": "ab92cd3b47d9b8a8603c",
+    "url": "js/app.59dcff79.js"
+  },
+  {
+    "revision": "581f8e57324ab5979edd",
+    "url": "js/about.00653a73.js"
+  },
+  {
+    "revision": "339680a3b1e3cc7ce7765a1c9a996ec6",
+    "url": "index.html"
+  },
+  {
+    "revision": "39d6ba613b9a4380037e",
+    "url": "css/chunk-vendors.e611593c.css"
+  },
+  {
+    "revision": "ab92cd3b47d9b8a8603c",
+    "url": "css/app.f14b2702.css"
+  }
+];
